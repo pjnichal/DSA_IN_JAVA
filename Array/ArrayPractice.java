6d7ea2b54bd31af55bfd40a19a608d4f7b390e90
Pravin Nichal <nichal.pravin2001@gmail.com>
2024-01-22 12:46:14 +0530

    commit

--- Array/ArrayPractice.java
+++ Array/ArrayPractice.java
@@ -6,8 +6,20 @@ public class ArrayPractice {
         int[] arr1;
         arr1 = new int[10];
         int[] arr2 = new int[10];
-        
-        System.out.println("Hello");
+        int[] arr3 = { 10, 20, 30, 50 };
+        int arr4[] = new int[] { 10, 30, 40 };
+        for (int i : arr4) {
+            System.out.println(i);
+        }
+        // changes value in original
+        int arr5[] = arr4;
+        arr5[0] = 300;
+        System.out.println(arr4[0]);
+        // doesnot change
+        int arr6[] = arr4.clone();
+        arr6[1] = 400;
+        System.out.println(arr4[1]);
+
     }
 
 }
