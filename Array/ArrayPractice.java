e95f4bf86483df4dcec19606cea5449cb602df9f
Pravin Nichal <nichal.pravin2001@gmail.com>
2024-01-22 12:40:31 +0530

    first commit

---
+++ Array/ArrayPractice.java
@@ -0,0 +1,13 @@
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+        int arr[];
+        arr = new int[10];
+        int[] arr1;
+        arr1 = new int[10];
+        int[] arr2 = new int[10];
+        
+        System.out.println("Hello");
+    }
+
+}
